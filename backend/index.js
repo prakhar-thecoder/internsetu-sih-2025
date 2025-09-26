cd7ff80d36677fe9616ded82154fe284975cfbbd
prakhar-thecoder <pnp14072005@gmail.com>
2025-09-26 10:27:43 +0530

    feat: added cors to allow any origin

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'admin', 'views'));
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
